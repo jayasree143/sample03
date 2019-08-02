dd5500e730975c5a97e8193e1ed3e62ec803843e
jayasree143 <47857126+jayasree143@users.noreply.github.com>
2019-08-02 14:21:09 +0530

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,6 @@
+  function studentController($scope,$http) {
+            var url = "/data.txt";
+
+            $http.get(url).then( function(response) {
+               $scope.students = response.data;
+            });
